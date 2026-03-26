0ec9269ae00c6a01170611349820b138fb1cb690
nitesh maan <niteshmaan0032@gmail.com>
2026-03-26 13:47:01 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,8 @@ export const metadata: Metadata = {
   },
 };
 
+const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === "production";
+
 export default function RootLayout({
   children,
 }: {
@@ -45,55 +47,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* ✅ Google Tag Manager (ONLY on real domain) */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var allowedDomain = 'zonicllc.com';
-                var currentHost = window.location.hostname;
-
-                if (currentHost === allowedDomain) {
-                  (function(w,d,s,l,i){w[l]=w[l]||[];
-                  w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;
-                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                  f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-TSLH7NKW');
-                }
-              })();
-            `,
-          }}
-        />
+        {isProd && (
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TSLH7NKW');
+              `,
+            }}
+          />
+        )}
       </head>
 
       <body className={neueHaas.className}>
-        {/* ✅ GTM noscript (ONLY on real domain) */}
-        <Script
-          id="gtm-noscript"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var allowedDomain = 'zonicllc.com';
-                var currentHost = window.location.hostname;
-
-                if (currentHost === allowedDomain) {
-                  document.write(\`
-                    <noscript>
-                      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSLH7NKW"
-                      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-                    </noscript>
-                  \`);
-                }
-              })();
-            `,
-          }}
-        />
+        {isProd && (
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TSLH7NKW"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
 
         <Loader />
         <SmoothScroll>{children}</SmoothScroll>
