989141bd95730e48786ff7d0704e4b8b004dc9fa
nitesh maan <niteshmaan0032@gmail.com>
2026-03-26 12:02:01 +0530

    web

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -292,7 +292,26 @@ function Navbar() {
                             >
                               <span>View all services</span>
                               <span className="buttons__icon-wrapper">
-                                <svg viewBox="0 0 14 15" width="8">
+                                <svg
+                                  viewBox="0 0 14 15"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="buttons__icon-svg"
+                                  width="8"
+                                >
+                                  <path
+                                    d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                    fill="currentColor"
+                                  />
+                                </svg>
+
+                                <svg
+                                  viewBox="0 0 14 15"
+                                  fill="none"
+                                  width="8"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="buttons__icon-svg buttons__icon-svg--copy"
+                                >
                                   <path
                                     d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
                                     fill="currentColor"
