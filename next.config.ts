e5d3626b6d9d2e87329546e504af0c0d1ee4b36e
nitesh maan <niteshmaan0032@gmail.com>
2026-04-15 17:41:21 +0530

    url

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/service/google-my-business/gmb-suspension-reinstatement",
+        destination: "/services/gmb-reinstatement-help",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
